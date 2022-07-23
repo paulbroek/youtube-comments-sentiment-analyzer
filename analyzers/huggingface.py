27e923a6daca02a06674f0bdc40101937d3cf9a7
paulbroek <pauliusbroek@hotmail.com>
2022-07-23 23:01:28 +0200

    implement huggingface sentiment analyzer

---
+++ analyzers/huggingface.py
@@ -0,0 +1,9 @@
+"""transformers.py.
+
+Use HuggingFace transformers for sentiment analysis
+"""
+from transformers import pipeline
+
+sentiment_pipeline = pipeline("sentiment-analysis")
+data = ["I love you", "I hate you"]
+sentiment_pipeline(data)
